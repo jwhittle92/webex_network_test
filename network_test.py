5b396c4d994db9fa9ba6610a51aa3928a63fe828
jwhittle <jwhittle@ignw.io>
2020-02-28 10:48:16 -0800

    first version of script

--- network_test.py
+++ network_test.py
@@ -1,40 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from tkinter import Tk
 import time
+from datetime import datetime
+from selenium import webdriver
+from tkinter import Tk
+from twisted.internet import task, reactor
 
-driver = webdriver.Firefox()
-web = driver.get("https://mediatest.ciscospark.com")
-elem = driver.find_element_by_id('start_tests')
-elem.click()
+# How often the test runs in seconds
+interval = 300.0
 
-time.sleep(45)
+# Opens the webpage in FireFox, finds the 'Start Test' button, and clicks it. This assumes the user has a wired connection.
+def webex_test():
+    # Open FireFox and browse to the WebEx Network Test site ('mediatest.ciscospark.com')
+    driver = webdriver.Firefox()
+    driver.get("https://mediatest.ciscospark.com")
+    #Locate the 'Start Test' button and click it. This assumes the user is using the default '
+    elem = driver.find_element_by_id('start_tests')
+    elem.click()
 
-app = driver.find_element_by_class_name('card-result-text.card-result-text-app-ok').text
-room = driver.find_element_by_class_name('card-result-text.card-result-text-room-ok').text
-call = driver.find_element_by_class_name('card-result-text.card-result-text-call-ok').text
+    # Wait some time for the network tests to complete
+    time.sleep(45)
 
-lis = [app, room, call]
-print(lis)
+    # Locate the results after the tests have completed 
+    app = driver.find_element_by_class_name('card-result-text.card-result-text-app-ok').text
+    room = driver.find_element_by_class_name('card-result-text.card-result-text-room-ok').text
+    call = driver.find_element_by_class_name('card-result-text.card-result-text-call-ok').text
 
-if app == 'Successful' and room == 'Successful' and call == 'Successful':
-    print('The tests were successful')
-else:
-    # use the 'copy clipboard' function and save output to file
-    copypaste = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/article/div[5]/div/a[2]')
-    copypaste.click()
-    #store clipboard in variable
-    output = Tk().clipboard_get()
-    print(str(output))
-    #write variable to file
-    log = open('webexlogs.txt', 'w+')
-    log.write(output)
-    log.close()
+    # Evaluate the results of the network test
+    if app == 'Successful' and room == 'Successful' and call == 'Successful':
+        print('The tests were successful')
+    else:
+        print('The tests were not successful')
+        date = datetime.now()
+        # use the 'copy clipboard' function and save output to file
+        copypaste = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/article/div[5]/div/a[2]')
+        copypaste.click()
+        #store clipboard in variable
+        output = Tk().clipboard_get()
+        #write variable to file
+        log = open('webexlogs-' + str(date) +'txt', 'w+')
+        log.write(output)
+        log.close()
 
-#driver.quit()
- 
+    driver.quit()
+    return 'The test has completed'
 
 
-
+#Run the tests using specified interval
+schedule = task.LoopingCall(webex_test)
+schedule.start(interval)
+reactor.run()
